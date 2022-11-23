190c50b1023aea771c6f39b8e1856f44ac237202
Pedro Esparza <pesparza@sigma.alimentos.com>
2022-11-23 09:09:34 -0600

    se agrega archivo con el total de animes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as data from '../assets/files/getAnimes.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'pedrerete-animes';
+  data: any = (data as any).default;
+  animes:any
+  animeElements: string[] = ['ID','Name','Status','Score']
+  baseUrl = "https://myanimelist.net/anime/"
+  constructor(){}
+  ngOnInit(){
+    this.animes = this.data.data
+  }
 }
